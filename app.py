59fbab1d8e133db7d2669eb27960146014fa0998
Stevan Grubac <stevangrubac123@gmail.com>
2022-02-16 13:39:17 -0500

    commit

--- app.py
+++ app.py
@@ -1,19 +1,26 @@
-from flask import Flask, request, render_template, flash, redirect
+from flask import Flask, request, render_template, flash, redirect, session
 from surveys import Survey, satisfaction_survey
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "ooOOooOOooOoo-secret!"
 
-#stores survey responses
-responses = []
+
+
 
 
 @app.route("/")
 def home():
     return render_template("home.html")
 
+@app.route("/handleSession")
+def handleSession():
+    session['responses'] = []
+    return redirect('/question/0')
+
+
 
 @app.route("/question/<int:num>")
 def question_display(num):
+    responses = session['responses']
     if(len(responses) == len(satisfaction_survey.questions)):
         return redirect("/complete")
     elif(len(responses) != num):
@@ -27,7 +34,10 @@ def question_display(num):
 @app.route("/answer", methods={"POST"})
 def handle_answer():
     answer = request.form['choice']
+    #Handles session data
+    responses = session['responses']
     responses.append(answer)
+    session['responses'] = responses
     return redirect(f"question/{len(responses)}")
 
 
